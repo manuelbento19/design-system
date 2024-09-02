7fde5f28e885919abba3889b1bd938ece438601d
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-09-02 10:45:00 +0100

    feat: react components

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,17 +1,2 @@
-
-import { styled } from './styles';
-
-const Button = styled("button",{
-    background: "$black",
-    color: "$gray-100",
-    padding: "$1",
-    height: "$12",
-    borderRadius: "$sm"
-})
-
-export default function App(){
-
-    return(
-        <Button>MyButton</Button>
-    )
-}
+export * from './styles';
+export * from './components';
